1967474334b434720bed266354b5683a647a97e3
ksofa <106625288+ksofa@users.noreply.github.com>
2024-03-08 13:31:44 +0300

    Update pom

---
+++ src/main/java/org/example/Controllers/KeyController.java
@@ -0,0 +1,9 @@
+package org.example.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class KeyController {
+}
